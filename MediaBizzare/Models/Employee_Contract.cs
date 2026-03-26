199d2620cf795a90f0935c12ace0279d236cb2a2
adam110311 <70455243+adam110311@users.noreply.github.com>
2026-03-26 09:35:06 +0100

    repo change

---
+++ MediaBizzare/Models/Employee_Contract.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediaBizzare.Models
+{
+    public class Employee_Contract
+    {
+        public int Id { get; set; }
+        [Required]
+        public int employeeId { get; set; }
+        [Required]
+        public DateTime signature_date { get; set; }
+        [Required]
+        public DateTime start_date { get; set; }
+        [Required]
+        public DateTime end_date { get; set; }
+        [Required]
+        public int salary { get; set; }
+        [Required]
+        public int hours_per_week { get; set; }
+        [Required]
+        public string contract_type { get; set; }
+    }
+}
