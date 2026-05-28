81edf12d0af007d72df08da8129ced96d6407b88
dimitriknoops <dimitriknoops@gmail.com>
2026-05-28 10:23:43 +0200

    Add initial project configuration files and database migration for product variations

---
+++ MediaBizzare/Migrations/20260528080612_EnforceDepartmentCategoryOneToOne.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediaBizzare.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnforceDepartmentCategoryOneToOne : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_DepartmentId",
+                table: "Categories");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_DepartmentId",
+                table: "Categories",
+                column: "DepartmentId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_DepartmentId",
+                table: "Categories");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_DepartmentId",
+                table: "Categories",
+                column: "DepartmentId");
+        }
+    }
+}
