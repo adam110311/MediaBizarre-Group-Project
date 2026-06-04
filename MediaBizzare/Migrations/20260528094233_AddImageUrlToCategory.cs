6234cf28603911a0f3370488c72c91b9ebec2d05
dimitriknoops <dimitriknoops@gmail.com>
2026-06-04 09:34:50 +0200

    Add ImageUrl column to Categories table for enhanced category representation

---
+++ MediaBizzare/Migrations/20260528094233_AddImageUrlToCategory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediaBizzare.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrlToCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Categories",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Categories");
+        }
+    }
+}
